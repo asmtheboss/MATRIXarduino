7dea05471693ae04a8b78158bcf2088d83c6c3ce
asmtheboss <asmtheboss@gmail.com>
2021-02-01 15:39:43 +0100

    int

--- src/lcd.h
+++ src/lcd.h
@@ -1,42 +1,69 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
+
 const int rs = PA9, en = PA10, d4 = PB6, d5 = PB7, d6 = PB8, d7 = PB9;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void inicio(){
-
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("   introduzca");
     lcd.setCursor(0,1);
-    lcd.print("    tarjeta");
-
+    lcd.print("   su tarjeta");
+    
 }
 
 void cargando(){
-    
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("espero por favor");
-    lcd.setCursor(0,1);
-    lcd.print("analizando...");
+
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("espere por favor");
+        lcd.setCursor(0,1);
+        lcd.print("analizando...");
+
 }
 
-void aceptada(){
+void aceptadaParte1(){
 
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("tarjeta aceptada");
     lcd.setCursor(0,1);
-    lcd.print("puede pasar...");
+    lcd.print("por favor");
+}
+void aceptadaParte2(){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("recoja su ");
+    lcd.setCursor(0,1);
+    lcd.print("tarjeta");
 }
 
-void denegado(){
+void denegadoParte1(){
 
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("tarjeta denegada");
     lcd.setCursor(0,1);
     lcd.print("no valida");
+}
+
+void denegadoParte2(){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("recoja su ");
+    lcd.setCursor(0,1);
+    lcd.print("tarjeta");
+}
+
+void accesoNoPermitido(){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Acceso Irregular");
+    lcd.setCursor(0,1);
+    lcd.print("Vuelva Atras");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,357 @@
 #include <Arduino.h>
 #include <788BSmatrixSetup.h>
-#include <simbolos.h>
 #include <LCD.h>
 
 
 int a = 0;
-int boton = PB14;
+int botonCorrecto = PB14;
+int botonIncorrecto = PB15;
+int buzzer = PB13;
+int sensor = PB0;
+
+boolean lcdState1 = false;
+boolean matrixState1 = false;
+boolean aceptada1 = false;
+boolean denegada1 = false;
+boolean sensor1 = false;
+
+int aceptadaContador = 0;
+int denegadaContador = 0;
+
+unsigned long lcdBase = 0;
+unsigned long matrixBase = 0;
+unsigned long aceptadaBase = 0;
+unsigned long denegadaBase = 0;
+unsigned long accesoIrregularBase = 0;
+
+const long lcdStop = 100;
+const long matrixStop = 1;
+const long aceptadaStop = 1000;
+const long denegadaStop = 1000;
+const long accesoIrregularStop = 1000;
+
 
 void setup() {
   lcd.begin(16,2);
+  pinMode(buzzer, OUTPUT);
+  pinMode(sensor, INPUT);
   matrixSetup();
-  pinMode(boton, INPUT);
-
 }
 
 void loop() {
 
+  if(digitalRead(botonCorrecto) == HIGH && digitalRead(botonIncorrecto) == LOW){
+    aceptada1 = true;
+  }
+  if(digitalRead(botonCorrecto) == LOW && digitalRead(botonIncorrecto) == HIGH){
+    denegada1 = true;
+  }
+  if(digitalRead(sensor) == LOW && digitalRead(botonCorrecto) == LOW && digitalRead(botonIncorrecto) == LOW){
+    sensor1 = true;
+  }
 
+/*----------ACEPTADA----------*/
+
+  if( aceptada1 == true){
+
+    unsigned long aceptadaTimer = millis();
+
+    if(aceptadaContador <= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          aceptadaParte1();
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          tickFino();
+        }
+      }
+    }
+
+    if(aceptadaContador >= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          aceptadaParte2();
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          tickGrueso();
+        }
+      }
+    }
+
+    if(aceptadaContador == 5){
+      aceptada1 = false;
+      aceptadaContador = 0;
+    }
+    if(aceptadaTimer - aceptadaBase >= aceptadaStop){
+      aceptadaContador++;
+      aceptadaBase = aceptadaTimer;
+    }
+  }
+
+/*----------DENEGADA----------*/
+
+  if( denegada1 == true){
+
+    unsigned long denegadaTimer = millis();
+
+    if(denegadaContador <= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          denegadoParte1();
+          digitalWrite(buzzer, HIGH);
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          cruzFina();
+        }
+      }
+    }
+
+    if(denegadaContador >= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          denegadoParte2();
+          digitalWrite(buzzer,LOW);
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          cruzGruesa();
+        }
+      }
+    }
+
+    if(denegadaContador == 5){
+      denegada1 = false;
+      denegadaContador = 0;
+    }
+    if(denegadaTimer - denegadaBase >= denegadaStop){
+      denegadaContador++;
+      denegadaBase = denegadaTimer;
+    }
+  }
+
+/*----------SENSOR----------*/
+
+  if( sensor1 == true){
+
+    unsigned long denegadaTimer = millis();
+
+    if(denegadaContador <= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          denegadoParte1();
+          digitalWrite(buzzer, HIGH);
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          cruzFina();
+        }
+      }
+    }
+
+    if(denegadaContador >= 2.5){
+      
+      unsigned long lcdTimer = millis();
+      unsigned long matrixTimer = millis();
+
+      if(lcdTimer - lcdBase >= lcdStop){
+        lcdBase = lcdTimer;
+
+        if(lcdState1 == false){
+          lcdState1 = true;
+        }
+        else{
+          lcdState1 = false;
+        }
+
+        if(lcdState1 == true){
+          denegadoParte2();
+          digitalWrite(buzzer,LOW);
+        }
+      }
+      if(matrixTimer - matrixBase >= matrixStop){
+        matrixBase = matrixTimer;
+
+        if(matrixState1 == false){
+          matrixState1 = true;
+        }
+        else{
+          matrixState1 = false;
+        }
+
+        if(matrixState1 == true){
+          cruzGruesa();
+        }
+      }
+    }
+
+    if(denegadaContador == 5){
+      sensor1 = false;
+      denegadaContador = 0;
+    }
+    if(denegadaTimer - denegadaBase >= denegadaStop){
+      denegadaContador++;
+      denegadaBase = denegadaTimer;
+    }
+  }
+
+/*----------INICIO----------*/
+
+  if(aceptada1 == false && denegada1 == false && sensor1 == false){
+
+    unsigned long lcdTimer = millis();
+    unsigned long matrixTimer = millis();
+
+    if(lcdTimer - lcdBase >= lcdStop){
+      lcdBase = lcdTimer;
+
+      if(lcdState1 == false){
+        lcdState1 = true;
+      }
+      else{
+        lcdState1 = false;
+      }
+
+      if(lcdState1 == true){
+        inicio();
+      }
+    }
+    if(matrixTimer - matrixBase >= matrixStop){
+      matrixBase = matrixTimer;
+
+      if(matrixState1 == false){
+        matrixState1 = true;
+      }
+      else{
+        matrixState1 = false;
+      }
+
+      if(matrixState1 == true){
+        flechaFina();
+      }
+    }
+  }
 }
+
+
+
+
+
+
